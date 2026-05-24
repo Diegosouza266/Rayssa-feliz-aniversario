205917a15c70a5867e196ea84e4948dcefc62e94
Diegosouza266 <148661003+Diegosouza266@users.noreply.github.com>
2026-05-24 19:12:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('btn-surpresa').addEventListener('click', function() {
+    alert('Parabéns, Rayssa! Que seu dia seja tão incrível e único quanto você. Continue sendo essa força inabalável!');
+});
